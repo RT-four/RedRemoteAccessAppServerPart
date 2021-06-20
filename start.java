1a4baeebfecdf51e640b9efdbd64c96d4d12b98f
Philipp <philvas13@gmail.com>
2021-06-20 11:24:43 +0300

    fdfd

--- start.java
+++ start.java
@@ -3,6 +3,11 @@
 
 public class start {
     public static void main(String[] args) {
+        try{
+            Runtime.getRuntime().exec("ssh -D 8181 phili@192.168.1.62");
+
+        }catch(Exception e){
+        }
         SetPassword frame1 = new SetPassword();
         frame1.setSize(300, 80);
         frame1.setLocation(500, 300);
